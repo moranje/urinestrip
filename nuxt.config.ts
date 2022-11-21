2deccd1933db6cf59507a867fd338b470a86ea47
Martien Oranje <martieno@gmail.com>
2022-11-21 23:52:27 +0100

    build: fix invalid import

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,32 @@
-import { defineNuxtConfig } from 'nuxt';
+// import legacy from '@vitejs/plugin-legacy';
+import { defineNuxtConfig } from 'nuxt/config';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  taget: 'static',
-
-  ssr: false,
+  // target: 'static',
 
   typescript: {
     shim: false,
+  },
+
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'nl',
+      },
+      charset: 'utf-16',
+      title: 'urinest.rip',
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'Een workflow voor urineonderzoek' },
+      ],
+      link: [{ rel: 'manifest`', href: 'manifest.json' }],
+      noscript: [
+        // <noscript>Javascript is required</noscript>
+        { children: 'Javascript is required' },
+      ],
+    },
   },
 
   css: [
@@ -18,6 +37,14 @@ export default defineNuxtConfig({
   tailwindcss: {
     configPath: 'tailwind.config.js',
   },
+
+  // vite: {
+  //   plugins: [
+  //     legacy({
+  //       targets: ['defaults', 'not IE 11']
+  //     })
+  //   ]
+  // },
 
   modules: [
     '@nuxtjs/tailwindcss',
